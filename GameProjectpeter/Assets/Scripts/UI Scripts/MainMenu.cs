9efaa673eed9e248dba7d1a6c4546e9f0e70303c
killersmurf96 <killersmurf93@gmail.com>
2018-10-21 17:39:14 +0100

    initial commit
    initial commit

---
+++ GameProjectpeter/Assets/Scripts/UI Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class MainMenu : MonoBehaviour {
+	//public	GameObject sound;
+	//public	AudioManager AM;
+
+	void Start()
+	{
+		
+	}
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void ShowReplay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void ExitGame()
+    {
+        //dosn't work in editor
+        Application.Quit();
+    }
+}
